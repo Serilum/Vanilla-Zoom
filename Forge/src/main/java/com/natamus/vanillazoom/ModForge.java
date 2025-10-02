f3c8c5f3026599a32e6bfb1ea81b1a77f25fd9d0
Rick South <rick@natamus.com>
2025-10-03 01:13:37 +0200

    (v2.9) Updated mod to use Collective's new RegisterKeyMappingsHelper.

--- Forge/src/main/java/com/natamus/vanillazoom/ModForge.java
+++ Forge/src/main/java/com/natamus/vanillazoom/ModForge.java
@@ -4,13 +4,9 @@ import com.natamus.collective.check.RegisterMod;
 import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.vanillazoom.forge.events.ForgeZoomEvent;
 import com.natamus.vanillazoom.util.Reference;
-import com.natamus.vanillazoom.util.Variables;
-import net.minecraft.client.KeyMapping;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -30,7 +26,7 @@ public class ModForge {
 
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> modEventBus.addListener(this::registerKeyBinding));
+		ModCommon.loadHotkeys();
 
 		modEventBus.addListener(this::loadComplete);
 
@@ -39,11 +35,6 @@ public class ModForge {
 
 		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
 	}
-
-	public void registerKeyBinding(final RegisterKeyMappingsEvent e) {
-    	Variables.hotkey = new KeyMapping("key.vanillazoom.togglezoom.desc", 342, "key.categories.misc");
-    	e.register(Variables.hotkey);
-    }
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
     	MinecraftForge.EVENT_BUS.register(ForgeZoomEvent.class);
