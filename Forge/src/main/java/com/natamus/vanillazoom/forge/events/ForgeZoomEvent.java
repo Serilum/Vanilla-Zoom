5494fa83475e1a6307b32b859593d7a7dce371a7
Rick South <rick@natamus.com>
2026-04-02 15:52:58 +0200

    Vanilla Zoom source code for 26.1.1.

---
+++ Forge/src/main/java/com/natamus/vanillazoom/forge/events/ForgeZoomEvent.java
@@ -0,0 +1,38 @@
+package com.natamus.vanillazoom.forge.events;
+
+import com.natamus.vanillazoom.events.ZoomEvent;
+import net.minecraft.world.InteractionResult;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeZoomEvent {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeZoomEvent.class);
+	}
+
+	@SubscribeEvent
+	public static void onClientTick(TickEvent.ClientTickEvent.Pre e) {
+		ZoomEvent.onClientTick();
+	}
+
+	@SubscribeEvent
+	public static boolean onItemUse(PlayerInteractEvent.RightClickItem e) {
+		if (ZoomEvent.onItemUse(e.getEntity(), e.getLevel(), e.getHand()).equals(InteractionResult.FAIL)) {
+			return true;
+		}
+		return false;
+	}
+
+	@SubscribeEvent
+	public static boolean onEntityInteract(PlayerInteractEvent.EntityInteractSpecific e) {
+		if (ZoomEvent.onEntityInteract(e.getEntity(), e.getLevel(), e.getHand(), e.getTarget(), null).equals(InteractionResult.FAIL)) {
+			return true;
+		}
+		return false;
+	}
+}
